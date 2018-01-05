85d3a30968f7ea3753d498957bc5e3f61844cdbb
Bob Walsh <bob.walsh@47hats.com>
2018-01-05 15:57:52 -0800

    typography.js working

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.scss'
+// import './index.scss'
+// has css that is overriding typography.js 
 
 const Header = () => (
   <div
---
+++ src/utils/typography.js
@@ -0,0 +1,13 @@
+import Typography from "typography";
+// previous theme:
+// import deYoungTheme from 'typography-theme-de-young'
+// const typography = new Typography(deYoungTheme)
+// or
+// import judahTheme from 'typography-theme-judah'
+// const typography = new Typography(judahTheme)
+
+// current theme:
+import moragaTheme from 'typography-theme-moraga'
+const typography = new Typography(moragaTheme)
+
+export default typography;
