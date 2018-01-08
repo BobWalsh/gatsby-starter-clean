ab15281579482b185587ad83401ebfd56ed8103a
Bob Walsh <bob.walsh@47hats.com>
2018-01-07 17:25:44 -0800

    working bootstrap 4

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,65 +1,112 @@
 import React from 'react'
+import { Container } from 'reactstrap'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-// import './index.scss'
 // has css that is overriding typography.js 
+import './index.scss'
 
-const Header = () => (
-  <div
-    style={{
-      background: '#0077CF',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          VS Code Mac, an opinionated Guide.
-        </Link>
-      </h1>
+const TemplateWrapper = ({ children, data }) => {
+  let user
+  if (typeof window !== 'undefined') {
+    user = window.netlifyIdentity && window.netlifyIdentity.currentUser()
+  }
+  return (
+    <div className='App'>
+      <Helmet title={data.site.siteMetadata.title} />
+      <div className='navbar navbar-expand-lg navbar-dark bg-primary'>
+        <Container>
+          <Link to='/' className='navbar-brand'>{data.site.siteMetadata.title}</Link>
+          <ul className='nav navbar-nav'>
+
+            {user && (
+              <li className='nav-item'>
+                <a href='/admin' className='nav-link'>Admin</a>
+              </li>
+            )}
+
+            <li className='nav-item'>
+              <Link to='/about' className='nav-link'>About</Link>
+            </li>
+          </ul>
+        </Container>
+      </div>
+      <div className='pageContent'>{children()}</div>
     </div>
-  </div>
-)
-
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  )
 }
 
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+}
+
+export const pageQuery = graphql`
+  query LayoutIndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
 export default TemplateWrapper
+
+// const Header = () => (
+//   <div
+//     style={{
+//       background: '#0077CF',
+//       marginBottom: '1.45rem',
+//     }}
+//   >
+//     <div
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 960,
+//         padding: '1.45rem 1.0875rem',
+//       }}
+//     >
+//       <h1 style={{ margin: 0 }}>
+//         <Link
+//           to="/"
+//           style={{
+//             color: 'white',
+//             textDecoration: 'none',
+//           }}
+//         >
+//           VS Code Mac, an opinionated Guide.
+//         </Link>
+//       </h1>
+//     </div>
+//   </div>
+// )
+
+// const TemplateWrapper = ({ children }) => (
+//   <div>
+//     <Helmet
+//       title="Gatsby Default Starter"
+//       meta={[
+//         { name: 'description', content: 'Sample' },
+//         { name: 'keywords', content: 'sample, something' },
+//       ]}
+//     />
+//     <Header />
+//     <div
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 960,
+//         padding: '0px 1.0875rem 1.45rem',
+//         paddingTop: 0,
+//       }}
+//     >
+//       {children()}
+//     </div>
+//   </div>
+// )
+
+// TemplateWrapper.propTypes = {
+//   children: PropTypes.func,
+// }
+
+// export default TemplateWrapper
